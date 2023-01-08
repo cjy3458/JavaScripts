8573d160c282c3612dfed7fda4908377ab9045ee
cjy3458 <cjy3458@naver.com>
2023-01-09 04:01:31 +0900

    JS

---
+++ last.js
@@ -0,0 +1,12 @@
+<body class="bg center">
+    <h1 class="title">2023년 신년계획</h1>
+    <p class="msg">매년 지키지못할 다짐은 NO!</p>
+    <div class="row flex-row wrap">
+      <div class="bucket img1-t2 center">들어가고싶은 곳에 합격하기</div>
+      <div class="bucket img2-t2 center done">웹 서비스 제작하기</div>
+      <div class="bucket img2-t2 center">운동하기..?</div>
+      <div class="bucket img1-t2 center">영어 공부하기</div>
+      <div class="bucket img1-t2 center done">금연하기</div>
+      <div class="bucket img2-t2 center">휴학하기</div>
+    </div>
+</body>
